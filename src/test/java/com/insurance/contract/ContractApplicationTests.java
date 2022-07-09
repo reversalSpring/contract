ff4187d8a771673ab7c95947c41a45a749b78af6
junhyungpplee <p_illusion@naver.com>
2022-07-10 02:22:14 +0900

    Initial commit

---
+++ src/test/java/com/insurance/contract/ContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
